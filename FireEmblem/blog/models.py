e8b8619e29e3e1f117c430b326a8eac743c8cb21
jiriquelme <67875173+jiriquelme@users.noreply.github.com>
2021-01-26 00:36:32 -0300

    testeo de la pagina principal

--- FireEmblem/blog/models.py
+++ FireEmblem/blog/models.py
@@ -13,8 +13,8 @@ class Publicacion(models.Model):
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.fecha_de_publicacion = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.titulo
--- FireEmblem/blog/views.py
+++ FireEmblem/blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_publicaciones(request):
+    return render(request, 'blog/lista_publicaciones.html', {})
